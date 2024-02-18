80bc696283459dd732ab4108280609082aa884fd
f0reachARR <f0reach@f0reach.me>
2024-02-19 01:08:43 +0900

    Add deb-s3 scripts

---
+++ buildroot/deb-s3/entrypoint.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+echo $INPUT_GPG_PRIVATE_KEY | gpg --import
+deb-s3 upload --bucket $INPUT_S3_BUCKET \
+    --access-key-id $INPUT_S3_ACCESS_KEY_ID \
+    --secret-access-key $INPUT_S3_ACCESS_KEY_SECRET \
+    --component main \
+    --endpoint $INPUT_S3_ENDPOINT \
+    --sign $INPUT_GPG_FINGERPRINT \
+    $INPUT_INPUT_DIR/*.deb
