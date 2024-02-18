45fc13353b5e526b678f6cd86b55f3437a017319
f0reachARR <f0reach@f0reach.me>
2024-02-18 16:46:20 +0900

    Update build script

--- create_debs/scripts/build.bash
+++ create_debs/scripts/build.bash
@@ -1,48 +1,37 @@
 #!/bin/bash
 
-pushd .
-apt update && apt upgrade -y 
-apt install -y python3-bloom fakeroot debhelper git gpg dh-python
+set -e
 
-pushd /
-git config --global url."https://x-access-token:$1@github.com/".insteadOf "https://github.com/"
-git clone https://github.com/ForteFibre/fortefibre_apt_repository.git
-gpg --dearmor -o /etc/apt/keyrings/fortefibre-key.gpg /fortefibre_apt_repository/public/public-key
-echo "deb [signed-by=/etc/apt/keyrings/fortefibre-key.gpg] file:///fortefibre_apt_repository/public jammy main " > /etc/apt/sources.list.d/fortefibre.list
-echo "yaml file:///fortefibre_apt_repository/public/rosdep/${ROS_DISTRO}_pkg.yaml" | sudo tee /etc/ros/rosdep/sources.list.d/50-my-packages.list
-apt update
-popd
+# TODO: Add apt repository for other packages
 
-bash /opt/ros/${ROS_DISTRO}/setup.bash 
-rosdep update 
-rosdep install -i --from-paths . -y 
+source /opt/ros/${ROS_DISTRO}/setup.bash 
 
-IFS=$'\n'
-PKG_LIST=$(colcon list -t)
-if [ -f ./my_rosdep.yaml ]; then
-    rm ./my_rosdep.yaml
-fi
+WORKSPACE_ROOT=$PWD
 
-for ITEM in $PKG_LIST; do
-    PKG_NAME=$(echo $ITEM | awk '{ print $1 }')
-    PKG_PATH=$(echo $ITEM | awk '{ print $2 }')
-    DEB_NAME=$(echo $PKG_NAME | sed -e 's/_/-/g' | sed -e 's/\(.*\)/\L\1/' | sed -e "s/^/ros-${ROS_DISTRO}-/g")
-    echo "$PKG_NAME:" >> ./my_rosdep.yaml
-    echo "  ubuntu: [$DEB_NAME]" >> ./my_rosdep.yaml
-done
+# Add local repository to rosdep
+colcon list -t -n | awk '{ pkg = $1; gsub("_", "-", pkg); print $1 ":\n  ubuntu: [ros-humble-" pkg "]" }' > rosdep.yaml
+cat rosdep.yaml
+echo "yaml file:///$PWD/rosdep.yaml" | sudo tee /etc/ros/rosdep/sources.list.d/50-my-packages.list -a
 
-echo "yaml file://$(pwd)/my_rosdep.yaml" | sudo tee /etc/ros/rosdep/sources.list.d/50-my-packages.list -a
+# Expected `rosdep init` has been done in the base image
+apt-get update
 rosdep update
 
-for ITEM in $PKG_LIST; do
-    PKG_NAME=$(echo $ITEM | awk '{ print $1 }')
-    PKG_PATH=$(echo $ITEM | awk '{ print $2 }')
-    DEB_NAME=$(echo $PKG_NAME | sed -e 's/_/-/g' | sed -e 's/\(.*\)/\L\1/' | sed -e "s/^/ros-${ROS_DISTRO}-/g")
+# Ensure that all dependencies are installed
+rosdep install -i --from-paths $WORKSPACE_ROOT/src -y
 
-    pushd $PKG_PATH
-    bloom-generate rosdebian
-    fakeroot debian/rules binary -j4
-    apt install -y ../$DEB_NAME*.deb
-    mv ../$DEB_NAME*.deb /debs
+export DEB_BUILD_OPTIONS="parallel=$(nproc) nocheck"
+
+for PKG_REL_PATH in $(colcon list -t -p); do
+    echo "Building $PKG_REL_PATH"
+    pushd $WORKSPACE_ROOT/$PKG_REL_PATH
+    rm -r .obj-* debian || true
+    bloom-generate rosdebian $WORKSPACE_ROOT/$PKG_REL_PATH
+    # Fix for some NVIDIA libraries
+    sed -i "s%dh_shlibdeps%dh_shlibdeps --dpkg-shlibdeps-params=--ignore-missing-info%" debian/rules
+    # Because container is running as root, fakeroot is not needed
+    ./debian/rules binary -j
+    dpkg -i --force-depends --force-downgrade ../*.deb
+    mv ../*.deb /debs
     popd
 done
