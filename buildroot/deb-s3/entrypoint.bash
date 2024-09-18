2447c883e25287b02f4d39a3a124ccbded8e8f94
f0reachARR <f0reach@f0reach.me>
2024-09-18 15:41:47 +0900

    Fix apt update script

--- buildroot/deb-s3/entrypoint.bash
+++ buildroot/deb-s3/entrypoint.bash
@@ -2,12 +2,11 @@
 
 set -e
 echo "$INPUT_GPG_PRIVATE_KEY" | gpg --import || true
-CODENAME=${INPUT_CODENAME:-jammy}
 deb-s3 upload --bucket $INPUT_S3_BUCKET \
     --access-key-id $INPUT_S3_ACCESS_KEY_ID \
     --secret-access-key $INPUT_S3_ACCESS_KEY_SECRET \
     --component main \
     --endpoint $INPUT_S3_ENDPOINT \
     --sign $INPUT_GPG_FINGERPRINT \
-    --codename $CODENAME \
+    --codename $INPUT_CODENAME \
     $INPUT_INPUT_DIR/*.deb
